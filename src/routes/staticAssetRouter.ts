e45ff4a91cee2ca0e8eedf36f6fdad6faff06800
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-10-21 14:11:40 +0300

    Add favicon

--- src/routes/staticAssetRouter.ts
+++ src/routes/staticAssetRouter.ts
@@ -6,6 +6,7 @@ const staticAssetRouter = express.Router()
 const bundleJsFilePath = path.resolve(`${__dirname}/../../.generated/bundle.js`)
 const bundleStylesFilePath = path.resolve(`${__dirname}/../../.generated/styles.css`)
 const indexHtml = path.resolve(`${__dirname}/../../index.html`)
+const favicon = path.resolve(`${__dirname}/../../client/img/Br.png`)
 
 const serveStaticResource = (filePath: string) => (req: express.Request, res: express.Response) => {
   res.sendFile(filePath)
@@ -14,5 +15,6 @@ const serveStaticResource = (filePath: string) => (req: express.Request, res: ex
 staticAssetRouter.get('/', serveStaticResource(indexHtml))
 staticAssetRouter.get('/bundle.js', serveStaticResource(bundleJsFilePath))
 staticAssetRouter.get('/styles.css', serveStaticResource(bundleStylesFilePath))
+staticAssetRouter.get('/favicon.ico', serveStaticResource(favicon))
 
 export default staticAssetRouter
