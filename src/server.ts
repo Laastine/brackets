189b2bec02e6715d75dde3cc7dc86bef4446b6f3
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-10-21 12:41:32 +0300

    Add CSS styles to build pipeline

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import * as path from 'path'
 const staticAssetRoute = express.Router()
 
 const bundleJsFilePath = path.resolve(`${__dirname}/../.generated/bundle.js`)
+const bundleStylesFilePath = path.resolve(`${__dirname}/../.generated/styles.css`)
 const indexHtml = path.resolve(`${__dirname}/../index.html`)
 
 const serveStaticResource = (filePath: string) => (req: any, res: any, next: any) => {
@@ -13,6 +14,7 @@ const serveStaticResource = (filePath: string) => (req: any, res: any, next: any
 
 staticAssetRoute.get('/', serveStaticResource(indexHtml))
 staticAssetRoute.get('/bundle.js', serveStaticResource(bundleJsFilePath))
+staticAssetRoute.get('/styles.css', serveStaticResource(bundleStylesFilePath))
 
 class Server {
   public app: express.Application
