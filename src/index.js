576cfd75182ae347379dde769251e4ef723dc326
Mikko Kaistinen <mikko.kaistinen@kapsi.fi>
2018-10-22 17:37:58 +0300

    Remove accidentally added js files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var server_1 = require("./server");
-var PORT = 4000;
-var server = new server_1["default"]();
-server.start(PORT);
-console.log("Server listening on port " + PORT);
